9d59c8f0e785f1f9eb2075f9becf0656d52f1d69
mateobode <mateo_bode@yahoo.com>
2025-07-09 09:12:22 +0900

    Select model based on CER, added early stopping, added weight decay for regularization

--- src/train.py
+++ src/train.py
@@ -3,6 +3,7 @@ import evaluate
 from functools import partial
 from transformers import Seq2SeqTrainingArguments, Seq2SeqTrainer, TrainerCallback, TrainingArguments, TrainerState, TrainerControl
 from transformers.trainer_utils import PREFIX_CHECKPOINT_DIR
+from transformers.trainer_callback import EarlyStoppingCallback
 from data_preprocessing import get_dataset, prepare_dataset
 from model import get_model_and_processor, DataCollatorNAMSeq2SeqPadding
 
@@ -27,7 +28,7 @@ class SavePeftModelCallback(TrainerCallback):
         return control
     
 
-def compute_metrics(pred, tokenizer, metric):
+def compute_metrics(pred, tokenizer, wer_metric, cer_metric):
     pred_ids = pred.predictions
     label_ids = pred.label_ids
 
@@ -37,9 +38,11 @@ def compute_metrics(pred, tokenizer, metric):
     # do not want to group tokens when computing the metrics
     pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
     label_str = tokenizer.batch_decode(label_ids, skip_special_tokens=True)
-    wer = 100 * metric.compute(predictions=pred_str, references=label_str)
+
+    wer = 100 * wer_metric.compute(predictions=pred_str, references=label_str)
+    cer = 100 * cer_metric.compute(predictions=pred_str, references=label_str)
     
-    return {"wer": wer}
+    return {"wer": wer, "cer": cer}
 
 
 def main():
@@ -61,33 +64,41 @@ def main():
         decoder_start_token_id=model.config.decoder_start_token_id,
     )
 
-    metric = evaluate.load("wer")
-    compute_metrics_fn = partial(compute_metrics, tokenizer=tokenizer, metric=metric)
+    wer_metric = evaluate.load("wer")
+    cer_metric = evaluate.load("cer")
+    compute_metrics_fn = partial(
+            compute_metrics, 
+            tokenizer=tokenizer, 
+            wer_metric=wer_metric, 
+            cer_metric=cer_metric
+        )
 
     # Define training arguments
     training_args = Seq2SeqTrainingArguments(
         output_dir="output",
-        per_device_train_batch_size=8,
-        gradient_accumulation_steps=2,  # increase by 2x for every 2x decrease in batch size
+        per_device_train_batch_size=4,
+        gradient_accumulation_steps=4,  # increase by 2x for every 2x decrease in batch size
         learning_rate=5e-5,
-        warmup_steps=500,
-        num_train_epochs=30,
+        warmup_ratio=0.1,
+        num_train_epochs=20,
         lr_scheduler_type="cosine",
         fp16=True,
         eval_strategy="steps",
-        per_device_eval_batch_size=8,
+        per_device_eval_batch_size=4,
         predict_with_generate=True,
         generation_max_length=128,
         max_grad_norm= 1.0,
-        save_steps=500,
-        eval_steps=500,
+        save_steps=300,
+        eval_steps=300,
         logging_steps=25,
         report_to=["tensorboard"],
         load_best_model_at_end=True,
-        metric_for_best_model="wer",
+        metric_for_best_model="cer",
         greater_is_better=False,
         remove_unused_columns=False,
         label_names=["labels"],
+        weight_decay=0.1,
+        save_total_limit=5,
     )
 
     # Initialize the trainer
@@ -99,7 +110,13 @@ def main():
         data_collator=data_collator,
         compute_metrics=compute_metrics_fn,
         processing_class=processor.feature_extractor,
-        callbacks=[SavePeftModelCallback],
+        callbacks=[
+            SavePeftModelCallback,
+            EarlyStoppingCallback(
+                early_stopping_patience=7, 
+                early_stopping_threshold=0.001
+            )
+        ],
     )
     model.config.use_cache = False  # Disable cache to avoid issues with gradient checkpointing
 
